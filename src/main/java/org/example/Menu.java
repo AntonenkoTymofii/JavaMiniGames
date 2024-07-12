48685be6305cd175f4e48426669e06cfca52ca22
AntonenkoTymofii <antonenkotimofii@gmail.com>
2024-07-13 00:23:10 +0300

    Create Menu class

---
+++ src/main/java/org/example/Menu.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import javax.swing.*;
+
+public class Menu extends JFrame {
+    public Menu(){
+        setTitle("Menu");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500, 500);
+        setLocationRelativeTo(null);
+
+    }
+}
