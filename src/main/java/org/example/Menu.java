062b298545b68e1fead607bf6757d8d7320edfde
AntonenkoTymofii <antonenkotimofii@gmail.com>
2024-09-04 16:00:29 +0300

    create menu with buttons

--- src/main/java/org/example/Menu.java
+++ src/main/java/org/example/Menu.java
@@ -1,13 +1,30 @@
 package org.example;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class Menu extends JFrame {
+
     public Menu(){
         setTitle("Menu");
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(500, 500);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setSize(250, 250);
         setLocationRelativeTo(null);
+        JButton snakeButton = new JButton("Snake Game");
+        snakeButton.addActionListener(e -> {
+            new SnakeWindow().setVisible(true);
+        });
+        JButton ticTacToeButton = new JButton("Tic Tac Toe Game");
+        ticTacToeButton.addActionListener(e -> {
 
+        });
+        JButton sudokuButton = new JButton("Sudoku Game");
+        sudokuButton.addActionListener(e -> {
+
+        });
+        setLayout(new GridLayout(3,1));
+        add(snakeButton);
+        add(ticTacToeButton);
+        add(sudokuButton);
     }
 }
