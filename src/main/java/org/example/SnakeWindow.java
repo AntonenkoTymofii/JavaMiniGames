7e42cb81e4405df9e00de84d708f38928fd0794a
AntonenkoTymofii <antonenkotimofii@gmail.com>
2024-09-04 16:01:05 +0300

    create window with future game

---
+++ src/main/java/org/example/SnakeWindow.java
@@ -0,0 +1,25 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SnakeWindow extends JDialog {
+
+    SnakeGame snake;
+    SnakeWindow(){
+        setTitle("Snake Game");
+        setModal(true);
+        setBounds(600, 200, 640, 640);
+        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        snake = new SnakeGame(this);
+        add(snake);
+    }
+
+    public SnakeGame getSnake() {
+        return snake;
+    }
+
+    public void setSnake(SnakeGame snake) {
+        this.snake = snake;
+    }
+}
